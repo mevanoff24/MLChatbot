1d1f54099e440a28720abd01bd7328523220caa9
Matt Evanoff <mevanoff24@gmail.com>
2018-11-16 09:03:52 -0800

    add doc-strings

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -3,23 +3,22 @@ from wtforms import Form, TextAreaField, validators
 import sys
 import os
 from pathlib import Path
-
-
 import pandas as pd 
 import numpy as np 
 
-
-
-
 sys.path.append(os.path.abspath("../"))
 
+# python only
 # from dialogue_manager_python import DialogueManager
+# all tags 
 from dialogue_manager import DialogueManager
+
 from config import * 
 from utils import *
 
 # DB_NAME = '../StackOverflow_python.db'
-DB_NAME = '../StackOverflow.db'
+# DB_NAME = '../StackOverflow.db'
+DB_NAME = '../StackOverflow_newline_score.db'
 
 
 # load model class 
@@ -34,39 +33,41 @@ class SubmissionForm(Form):
                     validators.length(min=1)])
 
 
-
 @app.route('/')
 def index():
-    
     form = SubmissionForm(request.form)
     return render_template('main.html', form=form)
 
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
-    
+    # connect to DB
     connection = sqlite3.connect(DB_NAME)
-    print(connection)
     c = connection.cursor()
-    print(c)
     form = SubmissionForm(request.form)
     if request.method == 'POST' and form.validate():
         question = request.form['question_form']
         print(question)
-        # do all 
+        # clean user question 
         clean_question = clean_text(question)
-        results, distances = dm.get_similar(clean_question, topk=10, load=True, save=False, return_dist=True)
+        # get most similar neighbors, distances, tags, and confidence scores 
+        results, distances, tags, scores = dm.get_similar(clean_question, topk=10, load=True, save=False, return_dist=True)
+        # get parent and comments from DB
         df_parent, df_comments = dm.get_comments(results, connection=connection)
+        # join df_parent and df_comments
         df = dm.merge_parent_comment(df_parent, df_comments, results, distances)
-        
-        
-
-#         df = pd.read_csv(OUTPUT_PATH/'final_df.csv')
-#         df = df[['title', 'comment_parent', 'comment_comment', 'distances']]
-        df = df[['title', 'comment_parent', 'comment_comment', 'distances', 'tags']]
+        df = dm.joined_comment_comment(df)
+        try:
+            # weigh and sort top responsese based on levenshtein distance, Tf-Idf similarity and symbolic n-grams with PCA.
+            df = dm.weigh_features(df, clean_question, tags, scores)
+        except Exception as e:
+            df = df
+        df = df[['title', 'comment_parent', 'distances', 'tags', 'combined_comments']]
         data = []
-        for title, parent_comment, answer, distance in zip(df['title'], df['comment_parent'], df['comment_comment'], df['distances']):
+        # to pass to view 
+        for title, parent_comment, answer, distance in zip(df['title'], df['comment_parent'], df['combined_comments'], df['distances']):
             data.append((title, parent_comment, answer, np.round(distance, 2)))
+        
         connection.close()
         return render_template('predict.html', 
                     tables=[df.to_html()], 
@@ -80,6 +81,6 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=PORT, debug=True)
+    app.run(host='0.0.0.0', port=PORT, debug=False)
 
 
--- utils.py
+++ utils.py
@@ -4,51 +4,68 @@ import pandas as pd
 from pathlib import Path
 import csv
 import pickle
-import annoy
 from datetime import datetime
 from collections import defaultdict
-import sqlite3
 import re
 import html
 from sklearn.metrics.pairwise import linear_kernel
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import MinMaxScaler
 
 
 from config import * 
 
 import nltk
 from nltk.corpus import stopwords
+# stopword set 
 stopwords_set = set(stopwords.words('english'))
 
 
 def avg_word_vectors(question, embeddings, dim):
+    """Compute the average word vector based on individual word vectors trained from StarSpace"""
     words_embedding = [embeddings[word] for word in question.lower().split() if word in embeddings]
+    # return 0s if no word included
     if not words_embedding:
         return np.zeros(dim)
     words_embedding = np.array(words_embedding).astype(np.float32)
+    # simple mean return
     return words_embedding.mean(axis=0)
 
+
 def average_tfidf_vectors(question, embeddings, dim, vect, idf_scores):
+    """Average the weighted word vectors trained from StarSpace with the weight of a word given by tf-idf"""
     # get idf weights
     split_question = [word for word in question.lower().split() if word in embeddings]
+    # return 0s if no word included
     if not split_question:
         return np.zeros(dim).astype(np.float32)
+    # initialize empty array
     words_embedding = np.zeros((dim, len(split_question))).astype(np.float32)
     for i, token in enumerate(split_question):
         if token in embeddings:
+            # get vector
             embed_score = embeddings[token]
         else: embed_score = 0
+        # get tf-idf score
         idf_score = idf_scores[token]
-        # word vectors multiply by their TF-IDF scores
-        words_embedding[:, i] = embed_score * idf_score    
+        # word vectors weighted by their TF-IDF scores
+        words_embedding[:, i] = embed_score * idf_score 
+    # mean return 
     return words_embedding.mean(axis=1)
 
+
 def avg_word_vectors_OOV(question, embeddings, fasttext_embeddings, dim):
-    
+    """
+    Compute the average word vector based on individual word vectors trained from FastText
+    If the word is not included in the FastText word vectors we find the most similar vector from FastText
+    """
     words_embedding = []
     for word in question.lower().split():
         if word in embeddings:
             words_embedding.append(embeddings[word])
         else:
+            # compute similarity between this unknown word and other FastText word vectors
             cosine_similarities = linear_kernel([fasttext_embeddings[word]], np.array(word_list)[:, 1:])
             related_docs_indices = cosine_similarities.argsort()[:-10:-1]
             words_embedding.append(word_list[related_docs_indices[0][0]][1:])
@@ -56,10 +73,15 @@ def avg_word_vectors_OOV(question, embeddings, fasttext_embeddings, dim):
     if not words_embedding:
         return np.zeros(dim)
     words_embedding = np.array(words_embedding).astype(np.float32)
+    # mean return 
     return words_embedding.mean(axis=0)
 
 
 def average_tfidf_vectors_OOV(question, embeddings, fasttext_embeddings, dim, vect, idf_scores):
+    """
+    Compute the tf-idf weighted average word vector based on individual word vectors trained from FastText
+    If the word is not included in the FastText word vectors we find the most similar vector from FastText
+    """
     split_question = [word for word in question.lower().split()]
     
     if not split_question:
@@ -71,6 +93,7 @@ def average_tfidf_vectors_OOV(question, embeddings, fasttext_embeddings, dim, ve
         if token in embeddings:
             embed_score = embeddings[token]
         else: 
+            # compute similarity between this unknown word and other FastText word vectors
             cosine_similarities = linear_kernel([fasttext_embeddings[token]], np.array(word_list)[:, 1:])
             related_docs_indices = cosine_similarities.argsort()[:-10:-1]
             embed_score = word_list[related_docs_indices[0][0]][1:]
@@ -79,11 +102,13 @@ def average_tfidf_vectors_OOV(question, embeddings, fasttext_embeddings, dim, ve
             idf_score = idf_scores[token]
         else: idf_score = 1
         # word vectors multiply by their TF-IDF scores
-        words_embedding[:, i] = embed_score * idf_score    
+        words_embedding[:, i] = embed_score * idf_score 
+    # mean return
     return words_embedding.mean(axis=1)
 
 
 def get_embeddings(filename, array=False):
+    """Load Word Embedding into memory. Return dictionary and embedding dim"""
     embeddings = {}
     with open(MODEL_PATH/filename, newline='') as f:
         reader = csv.reader(f, delimiter='\t')
@@ -97,29 +122,24 @@ def get_embeddings(filename, array=False):
     return embeddings, dim
 
 
-
-# def get_embeddings(filename):
-#     embeddings = {}
-#     with open(MODEL_PATH/filename, newline='') as f:
-#         reader = csv.reader(f, delimiter='\t')
-#         embed_list = list(reader)
-#     for line in embed_list:
-#         embeddings[line[0]] = np.asarray(line[1:], dtype=np.float32)
-        
-#     dim = len(embeddings['code'])
-#     return embeddings, dim
-
-
 def unpickle(filename):
+    """Unpickle file"""
     with open(filename, 'rb') as f:
         return pickle.load(f)
 
 
-
 re1 = re.compile(r'  +')
-# def clean_title(text, remove_html=False, other=False):
 def clean_text(text, remove_html=False, other=False):
+    """
+    Clean text field. Remove puctuations, weird chars, stopwords
     
+    Args:
+        text (str): text
+        remove_html (boolean): Remove all 'code' aspects of text if True
+    
+    Returns:
+        text (str): Cleaned text
+    """
     replace_by_space_re = re.compile('[/(){}\[\]\|@,;]')
     bad_symbols_re = re.compile('[^0-9a-z #+_]')
     
@@ -138,26 +158,99 @@ def clean_text(text, remove_html=False, other=False):
     return re1.sub(' ', html.unescape(text).strip())
 
 
-
 def get_top_preds(X, clf, class_map, k=3, cutoff=0.8):
-        preds = clf.predict_proba(X)
-        sorted_preds = np.argsort(preds)[:,::-1][0][:3]
-        top_scores = [preds[0][i] for i in sorted_preds]
-        if top_scores[0] > cutoff:
-            # if top_scores above threshold only need to return top 
-            return [class_map[sorted_preds[0]]]
-        else:
-            return [class_map[p] for p in sorted_preds]
+    """
+    Predict and return the top k predictions and confidence scores 
+    from Tag Classifier 
+    
+    Args:
+        clf (object): sklearn model with `predict_proba` attribute
+        class_map (dict): Sklearn label encoding mapping of class index to class name
+        k (int): number of predictions to return
+        cutoff (float): If model is this confident, we only return top1 prediction
+    """
+    # predict probabilities 
+    preds = clf.predict_proba(X)
+    # sort by top k
+    sorted_preds = np.argsort(preds)[:,::-1][0][:k]
+    top_scores = [preds[0][i] for i in sorted_preds]
+    print(top_scores)
+    if top_scores[0] > cutoff:
+        # if top_scores above threshold only need to return top 
+        return [class_map[sorted_preds[0]]], top_scores
+    else:
+        return [class_map[p] for p in sorted_preds], top_scores
 
 
-def tfidf_cosine_similarities(X, question, topk=5):
-    tfid_vectorizer = unpickle(MODEL_PATH/'tf_idf_python_title_stopwords.pkl')
+def joined(row):
+    """Append row to numpy array"""
+    arr = []
+    return np.append(arr, row)
 
-    tfidf_X = tfid_vectorizer.transform(X)
-    tfidf_question = tfid_vectorizer.transform([question])
 
-    cosine_similarities = linear_kernel(tfidf_question, tfidf_X).flatten()
-    related_docs_indices = cosine_similarities.argsort()[:-topk:-1]
-    print(related_docs_indices)
-    return final_df.loc[related_docs_indices]
+def compute_similarity(question, X):
+    """Compute the linear kernel between X and question"""
+    return linear_kernel(question, X).flatten()
 
+
+def matching_word(row, question):
+    """Return True if token (word) is in question"""
+    for token in question.split():
+        if token in row.split():
+            return True
+    return False
+           
+    
+def tfidf_cosine_similarities(df, col, question, topk=5):
+    """
+    Tf-idf fit on `col`, transform user question and compute linear kernel between `col` and question
+    Add column to `df` and return `df`
+    """
+    tfid_vectorizer = TfidfVectorizer(token_pattern='(\S+)', min_df=1, max_df=0.9, ngram_range=(1,1))
+    tfidf_X = tfid_vectorizer.fit_transform(df[col])
+    tfidf_question = tfid_vectorizer.transform([question]).todense()
+    df['tfidf_' + col] = compute_similarity(tfidf_question, tfidf_X)
+    return df
+
+
+def symbolic_ngrams(df, col, question, pca_dim=50, topk=5):
+    """
+    Fit BOW and PCA model on `col`, transform user question, and compute linear kernel between `col` and question
+    Add column to `df` and return `df`
+    """
+    vect = CountVectorizer(analyzer='char', ngram_range=(1, 5), min_df=1)
+    X = vect.fit_transform(df[col]).todense()
+    pca = PCA(pca_dim)
+    pca_X = pca.fit_transform(X)
+    vect_question = pca.transform(vect.transform([question]).todense())
+    df['ngram_' + col] = compute_similarity(vect_question, pca_X)
+    return df
+
+def tfidf_symbolic_ngrams_cosine_similarities(df, col, question, pca_dim=50, topk=5):
+    """
+    Fit tf-idf, BOW, and PCA model on `col`, transform user question, 
+    Compute linear kernel between `col` and question
+    Add column to `df` and return `df`
+    """
+    # tfidf 
+    tfid_vectorizer = TfidfVectorizer(token_pattern='(\S+)', min_df=1, max_df=0.9, ngram_range=(1,1))
+    tfidf_X = tfid_vectorizer.fit_transform(df[col]).todense()
+    
+    # symbolic n-grams (1 - 5)
+    vect = CountVectorizer(analyzer='char', ngram_range=(1, 5), min_df=1, max_df=0.9)
+    count_X = vect.fit_transform(df[col]).todense()
+    X = np.concatenate((tfidf_X, count_X), axis=1)
+    
+    # PCA 
+    pca = PCA(pca_dim)
+    pca_X = pca.fit_transform(X)
+    
+    # question
+    tfidf_question = tfid_vectorizer.transform([question]).todense()
+    count_question = vect.transform([question]).todense()
+    transformed_question = np.concatenate((tfidf_question, count_question), axis=1)
+    transformed_question = pca.transform(transformed_question)
+    
+    # cosine similarity 
+    df['tfidf_ngram_combo_' + col] = compute_similarity(transformed_question, pca_X)
+    return df
